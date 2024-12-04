321aaee7dcdc9a428e5ed80708fea812e02a5ac5
sigrid <sigrid.asling@gmail.com>
2024-12-04 13:41:18 +0100

    Add project files.

---
+++ Lab4KonsumeraWeb-API/Program.cs
@@ -0,0 +1,31 @@
+﻿using Lab4KonsumeraWeb_API;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; Lab4App)");
+
+        // Hämta JSON-data från GitHub API
+        var response = await client.GetAsync("https://api.github.com/orgs/dotnet/repos");
+        var content = await response.Content.ReadAsStringAsync();
+
+        // Deserialisera JSON till en lista av Repository-objekt
+        var repositories = JsonSerializer.Deserialize<List<Repository>>(content);
+
+        // Skriv ut resultaten
+        foreach (var repo in repositories)
+        {
+            Console.WriteLine(repo);
+        }
+    }
+}
+
+
+
---
+++ Lab4KonsumeraWeb-API/Repository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace Lab4KonsumeraWeb_API
+{
+    
+
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public string HtmlUrl { get; set; }
+
+        [JsonPropertyName("homepage")]
+        public string Homepage { get; set; }
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public DateTime PushedAt { get; set; }
+
+        public override string ToString()
+        {
+            return $"\nName: {Name}\nDescription: {Description}\nURL: {HtmlUrl}\n" +
+                   $"Homepage: {Homepage}\nWatchers: {Watchers}\nLast Pushed: {PushedAt}\n";
+        }
+    }
+
+
+
+}
